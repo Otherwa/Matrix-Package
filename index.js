182cfcb228948d42262665a248c7d57df08d7b6b
Atharv Desai <atharvdesai2002@gmail.com>
2023-07-07 12:20:16 +0000

    /minor changes/

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-const Math = require("./Mafs/Maths/Math")
-module.exports = { Math }
+const LinearEquations = require("./Mafs/Maths/LinearEquations/LinearEquations");
+const MensurationEquations = require("./Mafs/Maths/MensurationEquations/MensurationEquations");
+const QuadraticEquations = require("./Mafs/Maths/QuadraticEquations/QuadraticEquations");
+
+
+class Math {
+    constructor() {
+        this.linearEquations = new LinearEquations();
+        this.quadraticEquations = new QuadraticEquations();
+        this.mensurationEquations = new MensurationEquations();
+    }
+}
+
+module.exports =  Math;
